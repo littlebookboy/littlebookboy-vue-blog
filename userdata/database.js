b4e5b6190fdd42650bd2f84c46fe0ff67f52eef1
gene <gene@js-tech.tw>
2017-06-26 23:08:38 +0800

    更新關於我

--- userdata/database.js
+++ userdata/database.js
@@ -4,7 +4,7 @@ window.VUELOG_DATABASE = {
 
   config: {
     // The name of your site, will be displayed in browser tab and site header.
-    brand: {'en-US': 'Littlebookboy Blog', 'zh-CN': 'Littlebookboy 筆記'},
+    brand: { 'en-US': 'Littlebookboy Blog', 'zh-CN': 'Littlebookboy 筆記' },
 
     // The image displayed in site header right beside the brand.
     // logo: './static/vuelog.svg',
@@ -19,7 +19,7 @@ window.VUELOG_DATABASE = {
 
     // The path to route to when you visit `/`.
     // Set to `/home`, `/blog` or a valid path at your need.
-    homePath: '/home',
+    homePath: '/blog',
 
     // Whether footer is visible on `homePath` or not.
     homeFooter: false,
@@ -46,55 +46,40 @@ window.VUELOG_DATABASE = {
     livereUid: '',
 
     // Options for marked, see https://github.com/chjj/marked#options-1 for detail
-    markedOptions: {},
+    markedOptions: {}
 
   },
 
   navigation: [
     {
-      label: {'en-US': 'About Me', 'zh-CN': '關於我'},
+      label: { 'en-US': 'About Me', 'zh-CN': '關於我' },
       type: 'page',
       path: '/page/about-me'
     },
     {
-      label: {'en-US': 'Guide', 'zh-CN': '指南'},
-      type: 'page',
-      path: '/page/guide'
-    },
-    {
-      label: {'en-US': 'About', 'zh-CN': '关于'},
-      type: 'page',
-      path: '/page/about'
-    },
-    {
-      label: {'en-US': 'Showcase', 'zh-CN': '示例'},
-      type: 'category',
-      path: '/blog/showcase'
-    },
-    {
-      label: {'en-US': 'Archive', 'zh-CN': '归档'},
+      label: { 'en-US': 'Archive', 'zh-CN': '封存' },
       type: 'archive',
       path: '/archive'
     },
     {
-      label: {'en-US': 'Links', 'zh-CN': '链接'},
+      label: { 'en-US': 'Links', 'zh-CN': '社群' },
       type: 'dropdown',
       path: '', // (OPTIONAL) dropdown can be routable too if you set a valid route path
       children: [
         {
-          label: {'en-US': 'Weibo', 'zh-CN': '微博'},
+          label: { 'en-US': 'GitHub', 'zh-CN': 'GitHub' },
           type: 'outgoing',
-          link: 'http://weibo.com/myst729'
+          link: 'https://github.com/littlebookboy'
         },
         {
-          label: 'GitHub',
+          label: { 'en-US': 'Packagist', 'zh-CN': 'Packagist' },
           type: 'outgoing',
-          link: 'https://github.com/myst729'
+          link: 'https://packagist.org/packages/littlebookboy'
         },
         {
-          label: 'StackOverflow',
+          label: { 'en-US': 'Medium', 'zh-CN': 'Medium' },
           type: 'outgoing',
-          link: 'https://stackoverflow.com/users/1032492'
+          link: 'https://medium.com/@kevin2pig'
         }
       ]
     }
@@ -102,7 +87,7 @@ window.VUELOG_DATABASE = {
 
   pages: [
     {
-      title: {'en-US': 'About Me', 'zh-CN': '關於我'},
+      title: { 'en-US': 'About Me', 'zh-CN': '關於我' },
       slug: 'about-me',
       exclude: true,
       titleless: true,
@@ -110,7 +95,7 @@ window.VUELOG_DATABASE = {
       draft: false
     },
     {
-      title: {'en-US': 'Vuelog Guide', 'zh-CN': 'Vuelog 指南'},
+      title: { 'en-US': 'Vuelog Guide', 'zh-CN': 'Vuelog 指南' },
       slug: 'guide',
       exclude: true,      // (OPTIONAL) `true` to exclude the page from archive view
       titleless: false,   // (OPTIONAL) `true` to hide the title in page view
@@ -118,22 +103,22 @@ window.VUELOG_DATABASE = {
       draft: false        // (OPTIONAL) `true` to make the page temporarily inaccessible
     },
     {
-      title: {'en-US': 'About Vuelog', 'zh-CN': '关于 Vuelog'},
+      title: { 'en-US': 'About Vuelog', 'zh-CN': '关于 Vuelog' },
       slug: 'about'
     },
     {
-      title: {'en-US': 'Changelog', 'zh-CN': '变更日志'},
+      title: { 'en-US': 'Changelog', 'zh-CN': '变更日志' },
       slug: 'changelog'
     }
   ],
 
   categories: [
     {
-      title: {'en-US': 'Documentation', 'zh-CN': '文档'},
+      title: { 'en-US': 'Documentation', 'zh-CN': '文档' },
       slug: 'docs'
     },
     {
-      title: {'en-US': 'Showcase', 'zh-CN': '示例'},
+      title: { 'en-US': 'Showcase', 'zh-CN': '示例' },
       slug: 'showcase'
     }
   ],
@@ -141,57 +126,56 @@ window.VUELOG_DATABASE = {
   posts: [
     /* 2017 */
     {
-      title: {'en-US': 'Multiple languages support', 'zh-CN': '多语言支持'},
+      title: { 'en-US': 'Multiple languages support', 'zh-CN': '多语言支持' },
       slug: 'multiple-languages-support',
       category: 'docs',
       date: '2017-01-24'
     },
     {
-      title: {'en-US': 'Use social commenting services', 'zh-CN': '使用社会化评论服务'},
+      title: { 'en-US': 'Use social commenting services', 'zh-CN': '使用社会化评论服务' },
       slug: 'use-social-commenting-services',
       category: 'docs',
       date: '2017-01-19'
     },
     {
-      title: {'en-US': 'Insert code blocks with syntax highlight', 'zh-CN': '插入语法高亮的代码块'},
+      title: { 'en-US': 'Insert code blocks with syntax highlight', 'zh-CN': '插入语法高亮的代码块' },
       slug: 'insert-code-blocks-with-syntax-highlight',
       category: 'docs',
       date: '2017-01-18'
     },
     {
-      title: {'en-US': 'Author posts or pages', 'zh-CN': '编写文章或页面'},
+      title: { 'en-US': 'Author posts or pages', 'zh-CN': '编写文章或页面' },
       slug: 'author-posts-or-pages',
       category: 'docs',
       date: '2017-01-13'
     },
-
     {
-      title: {'en-US': 'Add posts or pages', 'zh-CN': '新建文章或页面'},
+      title: { 'en-US': 'Add posts or pages', 'zh-CN': '新建文章或页面' },
       slug: 'add-posts-or-pages',
       category: 'docs',
       date: '2017-01-12'
     },
     {
-      title: {'en-US': 'Customize navigation menu', 'zh-CN': '自定义导航菜单'},
+      title: { 'en-US': 'Customize navigation menu', 'zh-CN': '自定义导航菜单' },
       slug: 'customize-navigation-menu',
       category: 'docs',
       date: '2017-01-11'
     },
     {
-      title: {'en-US': 'Customize the site', 'zh-CN': '自定义站点'},
+      title: { 'en-US': 'Customize the site', 'zh-CN': '自定义站点' },
       slug: 'customize-the-site',
       category: 'docs',
       date: '2017-01-10'
     },
     /* 2016 */
     {
-      title: {'en-US': 'File structure of Vuelog', 'zh-CN': 'Vuelog 文件结构'},
+      title: { 'en-US': 'File structure of Vuelog', 'zh-CN': 'Vuelog 文件结构' },
       slug: 'file-structure-of-vuelog',
       category: 'docs',
       date: '2016-10-20'
     },
     {
-      title: {'en-US': 'Style examples', 'zh-CN': '样式示例'},
+      title: { 'en-US': 'Style examples', 'zh-CN': '样式示例' },
       slug: 'style-examples',
       category: 'showcase',
       date: '2016-10-19'
--- userdata/pages/about-me.md
+++ userdata/pages/about-me.md
@@ -10,27 +10,51 @@ I'm sorry there is no translation here for now.
 
 <!-- zh-CN:+ -->
 
+<img src="https://c1.staticflickr.com/5/4216/35509481666_71ddf3cb6d_m.jpg" width="240" height="240" alt="cutmypicr2">
+
+### 個人資訊
+
++ 名字：蘇靖淵
++ 生日：18/10/1987
++ 信箱：kevin2pig@gmail.com
+
+### 學歷
+
++ 國立勤益科技大學資訊工程系（所）碩士
++ 國立屏東商業技術學院（現屏東大學）資訊工程系學士
+
 ### 關於我
 
 把 code 寫好是我們做後端工程師的責任，寫文章打嘴砲是我本人的興趣，所以平常扶老太太過馬路，我每年二月三十都做一次，塵世中萬千迷途小書僮之一。
 
 土生土長的台灣人，曾短期在美國遊盪，出社會後寫 code 一年，接著到澳洲 WH，回台後繼續作為沒有羞恥心的軟體後端工程師糊口。
 
-### 略懂
+### 後端略懂
 
 + PHP
-+ MySQL
-+ Vue
-+ Git
-+ Javascript
-+ Laravel
-+ Codeingiter
-+ jQuery
-+ Redis
-+ teminal command
++ PHP framework: Codeigniter ,Laravel
++ MySQL, SQLite, Redis
 
-### 個人連結
+### 前端略懂
+
++ HTML5
++ CSS / SCSS, bootstrap
++ JavaScript, jQuery, Vue.js
+
+### 其他略懂
+
++ Git
++ Node.js
++ docker
++ apiDoc
++ Android, JNI, C++
++ Linux / Terminal Commands
++ Mac / Windows / Linux
+
+### 社群
 
 + [Github](https://github.com/littlebookboy)
++ [Packagist](https://packagist.org/packages/littlebookboy)
++ [Medium](https://medium.com/@kevin2pig)
 
 <!-- zh-CN:- -->
